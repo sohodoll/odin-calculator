f0ea37476900509268d8a863f32cc9111eb644f1
sohodoll <kovalevvtl@gmail.com>
2022-10-31 16:07:26 +0300

    feat: add basic JS functions and HTML boilerplate

---
+++ script.js
@@ -0,0 +1,21 @@
+const add = (a,b) => a+b;
+const subtract = (a,b) => a-b;
+const multiply = (a,b) => a*b;
+const divide = (a,b) => a/b;
+
+const operate = (operator, a, b) => {
+    if (operator == '+') {
+        return add(a,b)
+    }
+    if (operator == '-') {
+        return subtract(a,b)
+    }
+    if (operator == '*') {
+        return multiply(a,b)
+    }
+    if (operator == '/') {
+        return divide(a,b)
+    }
+}
+
+console.log(operate('/', 10, 5));
