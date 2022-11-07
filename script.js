ddb80ec652b9da4bda37aa1f474b58327cae5fbe
sohodoll <kovalevvtl@gmail.com>
2022-11-07 12:36:37 +0300

    feat: add basic keyboard support

--- script.js
+++ script.js
@@ -24,8 +24,16 @@ const operate = (operator, a, b) => {
 const buttons = Array.from(document.querySelectorAll('.calculator__button'));
 const display = document.querySelector('.calculator__display');
 const decimalButton = document.querySelector('.decimal');
+const undoButton = document.querySelector('.undo');
 
 let curNum = '';
+
+undoButton.addEventListener('click', () => {
+    if (curNum.length >= 1) {
+        curNum = curNum.slice(0, -1);
+        display.textContent = curNum;
+    }
+});
 
 const listenForButtons = () => {
     buttons.forEach(button => {
@@ -38,6 +46,17 @@ const listenForButtons = () => {
         })
     })
 }
+
+window.addEventListener('keydown', (event) => {
+    if (event.code.includes('Digit') && curNum.length < 9) {
+        curNum += event.key;
+        display.textContent = curNum;123
+    }
+    if (event.key == 'Backspace') {
+        curNum = curNum.slice(0, -1);
+        display.textContent = curNum;
+    }
+})
 
 listenForButtons();
 
@@ -62,16 +81,21 @@ const manageOperators = () => {
                 curOperator = operator.textContent;
                 firstNum = curNum;
                 curNum = '';
+                decimalButton.textContent = '.';
             } if (curOperator !== '' && curNum !== '') {
                     secondNum = curNum;
                     currentResult = operate(curOperator, firstNum, secondNum);
-                    firstNum = (Math.round(currentResult * 100) / 100).toFixed(2);
+                    firstNum = currentResult;
+                    if (currentResult % 1 != 0) {
+                        firstNum = (Math.round(currentResult * 100) / 100).toFixed(2);
+                    }
                     display.textContent = firstNum;
                     curNum = '';
+                    decimalButton.textContent = '.';
             }
             curOperator = operator.textContent;
             display.textContent = firstNum + curOperator;
-            if (currentResult == Infinity) {
+            if (currentResult == Infinity || isNaN(currentResult)) {
                 display.textContent = 'Nice try'
             };
         })
@@ -83,12 +107,16 @@ const manageEqual = () => {
         secondNum = curNum;
         if(firstNum && secondNum) {
             currentResult = operate(curOperator, firstNum, secondNum);
-            firstNum = (Math.round(currentResult * 100) / 100).toFixed(2);
+            firstNum = currentResult;
+            if (currentResult % 1 != 0) {
+                firstNum = (Math.round(currentResult * 100) / 100).toFixed(2);
+            }
             display.textContent = firstNum;
-            if (currentResult == Infinity) {
+            if (currentResult == Infinity || isNaN(currentResult)) {
                 display.textContent = 'Nice try'
             };
             curNum = '';
+            decimalButton.textContent = '.';
         };
     });
 };
@@ -102,6 +130,7 @@ const reset = () => {
     secondNum = '';
     currentResult = '';
     display.textContent = '';
+    decimalButton.textContent = '.';
 }
 
 const clearButton = document.querySelector('.clear');
